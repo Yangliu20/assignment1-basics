d1ba4024961bc9b93ac1b4f845f1b65883a2cf7e
Yang Liu <yangliu9921@gmail.com>
2025-05-22 05:43:50 +0000

    add ec2 setup script

---
+++ ec2_setup.sh
@@ -0,0 +1,9 @@
+sudo yum update -y
+sudo yum install git -y
+git --version
+git config --global user.email "yangliu9921@gmail.com"
+git config --global user.name "Yang Liu"
+
+sudo yum -y install python-pip
+pip install regex
+pip install uv
